89e0b630a45cca720f709e5e55fd71188aaa2c21
Vlad Ki <proger@wilab.org.ua>
2016-11-04 12:06:28 +0200

    Delete package.head.exs
    expm is no longer a thing.

--- package.head.exs
+++
@@ -1,5 +0,0 @@
-Expm.Package.new(name: "erlsh", description: "Erlang shell tools",
-                 version: :head, keywords: ["Erlang","shell","paths"], 
-                   maintainers: [[name: "Vladimir Kirillov", email: "proger@hackndev.com"]],
-                     repositories: [[github: "proger/erlsh"]],
-                     dependencies: [])
